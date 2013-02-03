a6dbfc6aeee2e5f898ebcda5a67b2f17792e3fe2
Simon Nilsson <sim.nilsson@gmail.com>
2013-02-03 03:32:59 +0100

    Disconnect provider handler on exit

--- WiiTUIO/ClientForm.xaml.cs
+++ WiiTUIO/ClientForm.xaml.cs
@@ -154,6 +154,7 @@ namespace WiiTUIO
         private void appWillExit(object sender, ExitEventArgs e)
         {
             this.disconnectProvider();
+            this.disconnectProviderHandler();
         }
 
 
