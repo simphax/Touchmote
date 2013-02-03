9a10610277fdf6d0721a95a588f4fa43f3c733c3
Simon Nilsson <sim.nilsson@gmail.com>
2013-02-03 04:30:17 +0100

    DriverInstall now installs driver

--- DriverInstall/MainWindow.xaml.cs
+++ DriverInstall/MainWindow.xaml.cs
@@ -18,26 +18,92 @@ using System.Windows.Shapes;
 
 namespace DriverInstall
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private string etd_SetviceName = "Tuio-To-vmulti-Device1";
-        private string etd_SetviceFilename = "Driver\\tuio_vmulti_service.exe";
+        private string etd_ServiceName = "Tuio-To-vmulti-Device1";
+        private string etd_ServiceFilename = "Driver\\tuio_vmulti_service.exe";
 
         public MainWindow()
         {
             InitializeComponent();
 
-
-            this.install_service(etd_SetviceName, etd_SetviceFilename,"3333");
-            this.give_service_permissions(etd_SetviceName);
         }
 
         private void consoleLine(string text) {
             this.console.Text += "\n";
             this.console.Text += text;
+        }
+
+        private void installAll()
+        {
+
+            this.installDriver();
+            this.install_service(etd_ServiceName, etd_ServiceFilename, "3333");
+            this.give_service_permissions(etd_ServiceName);
+        }
+
+        private void uninstallAll()
+        {
+
+            this.uninstallDriver();
+            this.uninstall_service(etd_ServiceName,etd_ServiceFilename);
+        }
+
+        private void installDriver()
+        {
+            try
+            {
+                //Devcon install vmultia.inf ecologylab\vmultia
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo();
+
+                procStartInfo.WorkingDirectory = System.AppDomain.CurrentDomain.BaseDirectory + "Driver\\";
+
+                procStartInfo.FileName = procStartInfo.WorkingDirectory + "devcon";
+                procStartInfo.Arguments = "install vmultia.inf ecologylab\\vmultia";
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                consoleLine(result);
+                proc.WaitForExit();
+            }
+            catch (Exception objException)
+            {
+                consoleLine(objException.Message);
+            }
+        }
+
+        private void uninstallDriver()
+        {
+            try
+            {
+                //Devcon remove *multi*
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo();
+
+                procStartInfo.WorkingDirectory = System.AppDomain.CurrentDomain.BaseDirectory + "Driver\\";
+
+                procStartInfo.FileName = procStartInfo.WorkingDirectory + "devcon";
+                procStartInfo.Arguments = "remove *vmulti*";
+
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                string result = proc.StandardOutput.ReadToEnd();
+                consoleLine(result);
+                proc.WaitForExit();
+            }
+            catch (Exception objException)
+            {
+                consoleLine(objException.Message);
+            }
         }
 
         //As described here
@@ -52,21 +118,17 @@ namespace DriverInstall
             SecurityIdentifier sid = user.User;
             consoleLine(sid.Value);
 
-            //sc sdshow Tuio-To-vmulti-Device1
             string service_permissions = sdshow_service(service_name);
             string permission_string = "(A;;RPWPCR;;;" + sid.Value + ")";
             consoleLine(permission_string);
-            //Get rid of the newlines
             service_permissions = service_permissions.Replace(Environment.NewLine, "");
-            //In case we have already set permissions
             service_permissions = service_permissions.Replace(permission_string,"");
-            //Add in the new permissions before the elevated permissions
             service_permissions = service_permissions.Replace("S:", permission_string + "S:");
 
-            consoleLine(service_permissions);
-            consoleLine(sdset_service(service_name, service_permissions));
+            sdset_service(service_name, service_permissions);
         }
 
+        //sc sdshow Tuio-To-vmulti-Device1
         public string sdshow_service(string service_name)
         {
             try
@@ -77,8 +139,6 @@ namespace DriverInstall
                 procStartInfo.FileName = "sc";
                 procStartInfo.Arguments = "sdshow " + service_name;
                 consoleLine("running " + procStartInfo.FileName + " " + procStartInfo.Arguments);
-                // The following commands are needed to redirect the standard output.
-                // This means that it will be redirected to the Process.StandardOutput StreamReader.
                 procStartInfo.RedirectStandardOutput = true;
                 procStartInfo.UseShellExecute = false;
                 procStartInfo.CreateNoWindow = true;
@@ -89,14 +149,12 @@ namespace DriverInstall
             }
             catch (Exception objException)
             {
-                
                 consoleLine(objException.Message);
                 return "";
-                // Log the exception
             }
         }
 
-        public string sdset_service(string service_name, string permission_string)
+        public void sdset_service(string service_name, string permission_string)
         {
             try
             {
@@ -106,28 +164,43 @@ namespace DriverInstall
                 procStartInfo.FileName = "sc";
                 procStartInfo.Arguments = "sdset " + service_name + " " + permission_string;
                 consoleLine("running " + procStartInfo.FileName + " " + procStartInfo.Arguments);
-                // The following commands are needed to redirect the standard output.
-                // This means that it will be redirected to the Process.StandardOutput StreamReader.
                 procStartInfo.RedirectStandardOutput = true;
                 procStartInfo.UseShellExecute = false;
                 procStartInfo.CreateNoWindow = true;
                 System.Diagnostics.Process proc = new System.Diagnostics.Process();
                 proc.StartInfo = procStartInfo;
                 proc.Start();
-                return proc.StandardOutput.ReadToEnd();
+                consoleLine(proc.StandardOutput.ReadToEnd());
             }
             catch (Exception objException)
             {
-
                 consoleLine(objException.Message);
-                return "";
-                // Log the exception
             }
         }
 
         private void install_service(string service_name, string file_name, string port)
         {
             consoleLine("Installing service : " + service_name + " : " + file_name + " , port : " + port);
+
+            this.uninstall_service(service_name,file_name);
+
+            //status = sc.Status.ToString();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = file_name;
+            startInfo.Arguments = "install " + port;
+            startInfo.UseShellExecute = true;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.CreateNoWindow = true;
+            using (Process exeProcess = Process.Start(startInfo))
+            {
+                exeProcess.WaitForExit();
+            }
+
+        }
+
+        private void uninstall_service(string service_name, string file_name)
+        {
+            consoleLine("Uninstalling service : " + service_name + " : " + file_name);
             //Process Process_Remove = new Process();
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = file_name;
@@ -141,23 +214,17 @@ namespace DriverInstall
             {
                 exeProcess.WaitForExit();
             }
-
-
-            //status = sc.Status.ToString();
-            ProcessStartInfo startInfo2 = new ProcessStartInfo();
-            startInfo2.FileName = file_name;
-            startInfo2.Arguments = "install " + port;
-            startInfo2.UseShellExecute = true;
-            startInfo2.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo2.CreateNoWindow = true;
-            using (Process exeProcess2 = Process.Start(startInfo2))
-            {
-                exeProcess2.WaitForExit();
-            }
-
         }
 
+        private void btnInstall_Click(object sender, RoutedEventArgs e)
+        {
+            this.installAll();
+        }
 
+        private void btnUninstall_Click(object sender, RoutedEventArgs e)
+        {
+            this.uninstallAll();
+        }
 
     }
 }
