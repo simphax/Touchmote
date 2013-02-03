474e201cb12644e4076952ecd0ddafe1f1223830
Simon Nilsson <sim.nilsson@gmail.com>
2013-02-03 11:55:41 +0100

    Disabled last feature for now

--- WiiTUIO/Input/WiiProvider/Pointer/WiiPointerProvider.cs
+++ WiiTUIO/Input/WiiProvider/Pointer/WiiPointerProvider.cs
@@ -282,7 +282,7 @@ namespace WiiTUIO.Provider
         public void start()
         {
 
-            Launcher.Launch("Driver", "enablewiimote.cmd", "", null);
+            //Launcher.Launch("Driver", "enablewiimote.cmd", "", null);
             // Ensure we cannot process any events.
             //pDeviceMutex.WaitOne();
 
@@ -343,7 +343,7 @@ namespace WiiTUIO.Provider
 
 
             //Disable and enable Wiimote in device manager to disconnect it from the computer
-            Launcher.Launch("Driver", "disablewiimote.cmd", "", null);
+            //Launcher.Launch("Driver", "disablewiimote.cmd", "", null);
 
             OnDisconnect(1);
         }
