dcd615713d936afdaaf79f0daff9f435d777bc58
Simon Nilsson <sim.nilsson@gmail.com>
2013-02-24 22:42:37 +0100

    Updated assembly info to beta 6

--- WiiTUIO/Properties/AssemblyInfo.cs
+++ WiiTUIO/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Touchmote")]
-[assembly: AssemblyDescription("v1.0b5")]
+[assembly: AssemblyDescription("v1.0b6")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Simphax")]
 [assembly: AssemblyProduct("Touchmote")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
