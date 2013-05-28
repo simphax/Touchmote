f826c74341a844ac4b7a524a9dc15930091927bb
Simon Nilsson <sim.nilsson@gmail.com>
2013-05-28 21:39:50 +0200

    Added temporary solution for a WPF/touch injection bug.

---
+++ ResetTouchInjection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResetTouchInjection
+{
+    class Program
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern Boolean InitializeTouchInjection(UInt32 maxCount, uint dwMode);
+
+        static void Main(string[] args)
+        {
+            InitializeTouchInjection(256, 1);
+        }
+    }
+}
