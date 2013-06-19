23685e183fffa0a01bd46714b50dcdd251da713b
Simon Nilsson <sim.nilsson@gmail.com>
2013-06-19 14:47:05 +0200

    Improved cursor rotation calculation

--- WiiTUIO/Input/WiiProvider/Pointer/ScreenPositionCalculator.cs
+++ WiiTUIO/Input/WiiProvider/Pointer/ScreenPositionCalculator.cs
@@ -11,10 +11,6 @@ namespace WiiTUIO.Provider
     public class ScreenPositionCalculator
     {
 
-        private PointF m_FirstSensorPos;
-        private PointF m_SecondSensorPos;
-        private PointF m_MidSensorPos;
-
         private int minXPos;
         private int maxXPos;
         private int maxWidth;
@@ -24,15 +20,20 @@ namespace WiiTUIO.Provider
         private int maxHeight;
         private int SBPositionOffset;
 
+        private double smoothedX, smoothedZ;
+        private int orientation;
+
+        private int leftPoint = -1;
+
         private System.Drawing.Rectangle screenBounds;
 
-        SmoothingBuffer rotationSmoothing;
+        SmoothingBuffer accelSmoothing;
 
         public ScreenPositionCalculator()
         {
             this.recalculateScreenBounds();
 
-            this.rotationSmoothing = new SmoothingBuffer(Settings.Default.pointer_rotationSmoothing);
+            this.accelSmoothing = new SmoothingBuffer(Settings.Default.pointer_rotationSmoothing);
 
             SystemEvents.DisplaySettingsChanged +=SystemEvents_DisplaySettingsChanged;
         }
@@ -63,6 +64,7 @@ namespace WiiTUIO.Provider
 
             PointF relativePosition = new PointF();
 
+            double rotation = 0;
             bool foundMidpoint = false;
 
             for(int i=0;i<irState.IRSensors.Count() && !foundMidpoint;i++)//IRSensor sensor in irState.IRSensors)
@@ -75,9 +77,56 @@ namespace WiiTUIO.Provider
                         IRSensor sensor2 = irState.IRSensors[j];
                         if (sensor2.Found)
                         {
+                            foundMidpoint = true;
+
                             relativePosition.X = (sensor.Position.X + sensor2.Position.X) / 2.0f;
                             relativePosition.Y = (sensor.Position.Y + sensor2.Position.Y) / 2.0f;
-                            foundMidpoint = true;
+
+                            if (Settings.Default.pointer_considerRotation)
+                            {
+                                smoothedZ = smoothedZ * 0.9 + args.WiimoteState.AccelState.RawValues.Z * 0.1;
+                                smoothedX = smoothedX * 0.9 + args.WiimoteState.AccelState.RawValues.X * 0.1;
+
+                                double absx = Math.Abs(smoothedX - 128), absz = Math.Abs(smoothedZ - 128);
+
+                                if (orientation == 0 || orientation == 2) absx -= 5;
+                                if (orientation == 1 || orientation == 3) absz -= 5;
+
+                                if (absz >= absx)
+                                {
+                                    if (absz > 5)
+                                        orientation = (smoothedZ > 128) ? 0 : 2;
+                                }
+                                else
+                                {
+                                    if (absx > 5)
+                                        orientation = (smoothedX > 128) ? 3 : 1;
+                                }
+
+                                int l = leftPoint, r;
+                                if (leftPoint == -1)
+                                {
+                                    switch (orientation)
+                                    {
+                                        case 0: l = (sensor.Position.X < sensor2.Position.X) ? 0 : 1; break;
+                                        case 1: l = (sensor.Position.Y > sensor2.Position.Y) ? 0 : 1; break;
+                                        case 2: l = (sensor.Position.X > sensor2.Position.X) ? 0 : 1; break;
+                                        case 3: l = (sensor.Position.Y < sensor2.Position.Y) ? 0 : 1; break;
+                                    }
+                                    leftPoint = l;
+                                }
+                                r = 1 - l;
+
+                                double dx = irState.IRSensors[r].Position.X - irState.IRSensors[l].Position.X;
+                                double dy = irState.IRSensors[r].Position.Y - irState.IRSensors[l].Position.Y;
+
+                                double d = Math.Sqrt(dx * dx + dy * dy);
+
+                                dx /= d;
+                                dy /= d;
+
+                                rotation = Math.Atan2(dy, dx);
+                            }
                         }
                     }
                 }
@@ -101,16 +150,17 @@ namespace WiiTUIO.Provider
                 offsetY = SBPositionOffset;
             }
 
-            double rotation = 0;
-
             relativePosition.X = 1 - relativePosition.X;
-
+            
             if (Settings.Default.pointer_considerRotation)
             {
-                rotationSmoothing.addValue(new System.Windows.Vector(args.WiimoteState.AccelState.Values.X, args.WiimoteState.AccelState.Values.Z));
-                System.Windows.Vector smoothedRotation = rotationSmoothing.getSmoothedValue();
+                //accelSmoothing.addValue(new System.Windows.Vector(args.WiimoteState.AccelState.Values.X, args.WiimoteState.AccelState.Values.Z));
 
-                rotation = -1 * (Math.Atan2(smoothedRotation.Y, smoothedRotation.X) - (Math.PI / 2.0));
+                
+
+                //System.Windows.Vector smoothedRotation = accelSmoothing.getSmoothedValue();
+
+                //rotation = -1 * (Math.Atan2(smoothedRotation.Y, smoothedRotation.X) - (Math.PI / 2.0));
 
                 relativePosition.X = relativePosition.X - 0.5F;
                 relativePosition.Y = relativePosition.Y - 0.5F;
@@ -119,6 +169,9 @@ namespace WiiTUIO.Provider
 
                 relativePosition.X = relativePosition.X + 0.5F;
                 relativePosition.Y = relativePosition.Y + 0.5F;
+
+                relativePosition.X = 1 - relativePosition.X;
+                relativePosition.Y = 1 - relativePosition.Y;
             }
 
             x = Convert.ToInt32((float)maxWidth * relativePosition.X + minXPos);
