cfa17963d7ec1a938cc1823bd57de9ded36c1e20
Simon Nilsson <sim.nilsson@gmail.com>
2013-11-24 13:35:06 +0100

    Redirected mouse output through keymap classes

--- WiiTUIO/Output/Handlers/ICursorHandler.cs
+++ WiiTUIO/Output/Handlers/ICursorHandler.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WiiTUIO.Provider;
 
 namespace WiiTUIO.Output.Handlers
 {
     interface ICursorHandler : IOutputHandler
     {
 
-        bool setPosition(string key, double x, double y);
+        bool setPosition(string key, CursorPos cursorPos);
 
     }
 }
